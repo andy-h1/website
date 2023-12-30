74abe504d457eeedae407260c664daaf2698e4bf
andy-h1 <a.huynh2612@gmail.com>
2023-12-30 01:10:18 +0000

    build: adding dependencies for website

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+    appDirectory: "app",
+    ignoredRouteFiles: ["**/.*"],
+    assetsBuildDirectory: "public/build",
+    serverModuleFormat: 'cjs',
+  };
+  
